49116be50e861109acdd7d1235661810bfaad156
Lyxc <aleixcult@gmail.com>
2025-03-02 00:27:30 -0400

    seconds minutes program (f) print formatting

--- Python_ basics.py
+++ Python_ basics.py
@@ -10,3 +10,21 @@
 # Python is a popular language for data analysis, with libraries like pandas, NumPy, and matplotlib.
 # Python is a popular language for machine learning, with libraries like scikit-learn, TensorFlow, and PyTorch.
 # automation with libraries like pyautogui, pywinauto, and pyautogui.
+
+# concatenate strings and numbers in print with format (f)
+print("20 days are " + str(28800) + " minutes")
+print(f"20 days are {28,800} minutes")
+print(f"20 days are {20 * 24 * 60} minutes")
+
+# Variables
+# separate multiple words with underline between them
+# Variables are case-sensitive
+
+dys = int(input("Enter a number of days: "))
+minutes = 24 * 60
+seconds = minutes * 60
+calcualtion_to_minutes = dys * minutes
+calculation_to_seconds = dys * seconds
+
+print(f"{dys} days are {calcualtion_to_minutes} minutes")
+print(f"{dys} days are {calculation_to_seconds} seconds")
