10b306c9e1d5badb7ac3af4205ee1c204774bc87
Lyxc <aleixcult@gmail.com>
2025-03-01 22:59:27 -0400

    intro to what python is and use areas

--- Python_ basics.py
+++ Python_ basics.py
@@ -1,1 +1,12 @@
 # Python basics
+# powerful language, many libraries:  web development with Django or Flask, data analysis with pandas, machine learning with scikit-learn, and more.
+# Python is an interpreted language, which means that it is not compiled like Java or C++.
+# Python is dynamically typed, which means that you don't have to declare the type of a variable when you create one.
+# Python is well suited for object-oriented programming.
+# Python is a high-level language, which means it abstracts from the computer, making it easier to write code.
+# Python is an interpreted language, which means that it is not compiled like Java or C++.
+# Python is dynamically typed, which means that you don't have to declare the type of a variable when you create one.
+# data collection with web scrapers libraries like BeautifulSoup, Scrapy, and Selenium.
+# Python is a popular language for data analysis, with libraries like pandas, NumPy, and matplotlib.
+# Python is a popular language for machine learning, with libraries like scikit-learn, TensorFlow, and PyTorch.
+# automation with libraries like pyautogui, pywinauto, and pyautogui.
