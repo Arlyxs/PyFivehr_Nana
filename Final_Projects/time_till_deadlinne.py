798c4d9246f4d19311da9c82e8a3dd45fa094c6d
Lyxc <aleixcult@gmail.com>
2025-03-05 01:07:53 -0400

    changed impot  definition

--- Final_Projects/time_till_deadlinne.py
+++ Final_Projects/time_till_deadlinne.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 user_input = input(
     "enter your goal with a deadline separated by colon\n e.g. learn python:12.04.2025"
@@ -9,8 +9,8 @@ goal = input_list[0]
 deadline = input_list[1]
 
 # calculate how many days from now till deadline
-deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
-today_date = datetime.datetime.today()
+deadline_date = datetime.strptime(deadline, "%d.%m.%Y")
+today_date = datetime.today()
 time_till = deadline_date - today_date
 hours_till = int(time_till.total_seconds() / 60 / 60)
 
@@ -23,5 +23,5 @@ print(f" Dear user! Time remaining till your goal: {goal}, is {hours_till} hours
 
 
 print(input_list)
-print(datetime.datetime.strptime(deadline, "%d.%m.%Y"))
+print(datetime.strptime(deadline, "%d.%m.%Y"))
 print(today_date)
