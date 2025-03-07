2db3b694d0ab66fb8cc1d1e965309a96911d1f0e
Lyxc <aleixcult@gmail.com>
2025-03-07 03:53:09 -0400

    ficed eureka celebration

--- Final_Projects/InventoryProject/invtest.py
+++ Final_Projects/InventoryProject/invtest.py
@@ -24,7 +24,7 @@ for product_row in range(2, product_list.max_row):  # last row excluded
         products_per_supplier[supplier_name] = 1
 
 print(products_per_supplier)
-# Working
+print("Working: dictionary written_(npi)")
 # ****************************************************************
 # calculation of total value  of inventory per supplier dict supplier:ttl_prod_val
 ttl_val_per_supplier = {}
@@ -48,29 +48,25 @@ for product_row in range(2, product_list.max_row):  # from first to last(incl)
         print(f"adding new supplier: {supplier_name} ")
 
 print(ttl_val_per_supplier)
-# working avoids cell multiplication
+print("working: avoids cell multiplication")
 # **********************************************************************
 
 ttl_val_per_supplier = {}
 
 for product_row in range(2, product_list.max_row):  # from first to last(incl)
-
+    supplier_name = product_list.cell(product_row, 4).value
     inventory = product_list.cell(product_row, 2).value
     price = product_list.cell(product_row, 3).value
-
-    supplier_name = product_list.cell(product_row, 4).value
     supp_row_val = inventory * price
     # supp_row_val = product_list.cell(product_row, 5).value
 
     if supplier_name in ttl_val_per_supplier:
-        current_product_value_supplier = ttl_val_per_supplier[supplier_name]
-        ttl_val_per_supplier[supplier_name] = (
-            current_product_value_supplier + supp_row_val
-        )
+        curr_prod_val_supplier = ttl_val_per_supplier[supplier_name]
+        ttl_val_per_supplier[supplier_name] = curr_prod_val_supplier + supp_row_val
     else:
         ttl_val_per_supplier[supplier_name] = supp_row_val
         print(f"adding new supplier: {supplier_name} ")
 
 print(ttl_val_per_supplier)
-# Working with cell multiplication
+print("Working: with cell multiplication")
 # *******************************************************************
