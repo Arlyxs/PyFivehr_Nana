50934a4c1f2a73201d1a5e9fc1f946d816ea3783
Arlyxs <arlyxs@gmail.com>
2025-03-11 16:45:50 -0400

    Update python_chatgpt.py
    changed apikey t o env variable

--- Final_Projects/PYTHON_CHATGPT/python_chatgpt.py
+++ Final_Projects/PYTHON_CHATGPT/python_chatgpt.py
@@ -3,4 +3,4 @@ import requests
 
 
 api_endpoint = "https://api.openai.com/v1/chat/completions"
-api_key = "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"
+api_key = os.getenv("OPENAI_API_KEY")
