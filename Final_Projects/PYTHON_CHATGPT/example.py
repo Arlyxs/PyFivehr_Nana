14591ba5fceb4efd17b2dd497974f03b6d2b168b
Lyxc <aleixcult@gmail.com>
2025-03-11 16:38:21 -0400

    error correciton

--- Final_Projects/PYTHON_CHATGPT/example.py
+++ Final_Projects/PYTHON_CHATGPT/example.py
@@ -1,7 +1,7 @@
 import os
 from openai import OpenAI, api_key
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))  # ?
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
--- Final_Projects/PYTHON_CHATGPT/python_chatgpt.py
+++ Final_Projects/PYTHON_CHATGPT/python_chatgpt.py
@@ -1,6 +1,14 @@
+import os
 from openai import api_key
 import requests
 
 
 api_endpoint = "https://api.openai.com/v1/chat/completions"
-api_key = "sk-proj-QFA2xfQg9SOV8t6tN04Q17hksO7ULeWVO48RklSTOnplVAOn4DSizQ1qKwjZBty6xAxQf0uPSJT3BlbkFJYiwH9Je8HPq1lNoAMh6a_uTVy7vW4WT3HYMKrhgHzqEkv6c2yBEK1UhkOC7Ccu0Zr4DBHMMnIAi"
+api_key=os.getenv("OPENAI_API_KEY") 
+request_headers = {"Content-Type: application/json", "Authorization: Bearer" + api_key}
+
+request_data{"model":}
+
+requests.post(api_endpoint,headers=request_headers, json{})
+
+
